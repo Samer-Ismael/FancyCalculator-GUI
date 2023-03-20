f51f442c316c7aa50a490466ff3971817ec20257
Samer Ismael <samerismaeelbat2@hotmail.com>
2023-03-20 21:37:00 +0100

    Pushing to Github

---
+++ src/main/java/com/mycompany/calculator/Calculator.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.calculator;
+/**
+ *
+ * @author Samer
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+      CalculatorGUI calc = new CalculatorGUI();
+      calc.setResizable(false);
+      
+      calc.setVisible(true);
+    }
+}
